f08cf59cea3ff5518b20439b883685dbf11bfbca
BhargavWebDeploy <srd05270@gmail.com>
2023-04-10 15:28:14 +0530

    Real Estate Project

---
+++ estate/__manifest__.py
@@ -0,0 +1,33 @@
+{
+    
+    'name':'Real Estate',
+    'version' : '1.1',
+    'author' : 'bhargav',
+    'summary' : 'Real Estate Project',
+    'sequence' : 1,
+    'category': 'project',
+    'description' : 'this is new real estate project',
+    
+    'depends' : ['base','sale','mail'],
+    
+    'data' : {
+        'security/ir.model.access.csv',
+        
+        'views/estate_menu.xml',
+        'views/property_view.xml',
+        'views/property_type_view.xml',
+        'views/property_offer_view.xml',
+        'views/property_tag_view.xml',
+        'views/user_property.xml',
+        'views/saleorder_field.xml',
+        
+        'report/report.xml',
+        'report/report_tamplate.xml',
+        
+        'data/mail_report_template.xml',
+        'data/mail_offer_accept_template.xml',
+        'data/mail_offer_reject_template.xml',
+        # 'data/ir_cron_template.xml',
+        
+    } 
+    }
---
+++ estate_account/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name':'Real Estate Account',
+    'version' : '1.1',
+    'author' : 'bhargav',
+    'summary' : 'Real Estate Account Project',
+    'sequence' : 1,
+    'category': 'Real Estate',
+    'description' : 'this is new real estate account project',
+    
+    'depends' : ['estate','account'],
+    
+    'data' : {
+    } 
+    
+    }
